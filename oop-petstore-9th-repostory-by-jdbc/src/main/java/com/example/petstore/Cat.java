2e90fa5811bcfb7517a101b3f620b3b121dfa79a
Jason <jasonecy@gmail.com>
2021-11-08 18:08:08 +0900

    Update pet store

---
+++ oop-petstore-9th-repostory-by-jdbc/src/main/java/com/example/petstore/Cat.java
@@ -0,0 +1,37 @@
+package com.example.petstore;
+
+
+import java.io.Serializable;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("cat")
+public class Cat extends Pet implements Groomable, Serializable, Runnable{
+
+    @Override
+    public void speak() {
+
+        if(getEnergy() < 5){            
+            System.out.println("I'm hungry");
+        }else     
+            System.out.println("Hi");
+    }
+
+    @Override
+    public String grooming() {
+        //answer must be obtained by UI
+
+        return "야옹. 행복하다 집사야.";
+    }
+
+    @Override
+    public void run() {
+        
+        System.out.println("I'm running!!!");
+        
+    }
+
+    
+}
